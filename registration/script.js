94dce3e18ba2099c862ddaac66543bc6f60d8d3f
Aloys Philip Otieno <aloysotieno69@gmail.com>
2026-04-01 11:04:33 +0300

    Update script.js

--- registration/script.js
+++ registration/script.js
@@ -3,38 +3,38 @@
 function registrationForm() {
     $("#registration_button").on("click", function() {
 
-    var $this = $("#send_otp_button"); //submit button selector using ID
+    var $this = $("#registration_button"); //submit button selector using ID
     var $caption = $this.html(); // We store the html content of the submit button
-    var form = "#send_otp_form"; //defined the #form ID
+    var form = "#registration_form"; //defined the #form ID
     var formData = $(form).serialize(); //serialize the form into array
     var route = $(form).attr('action'); //get the route using attribute action
     
-  //  var mobile = $("#signin-email").value;
+  //  var mobile = $("#phoneNumber").value;
   //  var phone = mobile.replaceAt(0, "254");
 
-      console.log("You pressed submit button");
+    console.log(phone);
 
     // Ajax config
-    $.ajax({
-        type: "POST", //we are using POST method to submit the data to the server side
-        url: route, // get the route value
-        data: formData, // our serialized array data for server side
-        beforeSend: function() { //We add this before send to disable the button once we submit it so that we prevent the multiple click
-            $this.attr('disabled', true).html("Processing...");
+ //   $.ajax({
+ //      type: "POST", //we are using POST method to submit the data to the server side
+//        url: route, // get the route value
+//        data: formData, // our serialized array data for server side
+ //       beforeSend: function() { //We add this before send to disable the button once we submit it so that we prevent the multiple click
+//            $this.attr('disabled', true).html("Processing...");
 
-        },
-        success: function(response) { //once the request successfully process to the server side it will return result here
-            $this.attr('disabled', false).html($caption);
+ //       },
+//        success: function(response) { //once the request successfully process to the server side it will return result here
+//            $this.attr('disabled', false).html($caption);
             
           //  alert(response);
           
-          window.location.href = 'https://sekondz.com/user/auth-2.html?mobile=' + response;
+ //         window.location.href = 'https://sekondz.com/user/auth-2.html?mobile=' + response;
 
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
+//        },
+  //      error: function(XMLHttpRequest, textStatus, errorThrown) {
             // You can put something here if there is an error from submitted request
-        }
-    });
+  //      }
+ //   });
 
     });
 }
@@ -43,8 +43,6 @@ function registrationForm() {
 $(window).load(function() {
 
     // Submit form using AJAX
-    sendOTP();
-    submitOTP();
-  //  checkUser();
+    registrationForm();
 
 });
