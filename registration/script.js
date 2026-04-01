c4237730de6fd6c087cf927d6ba9b0cfe31d72b9
Aloys Philip Otieno <aloysotieno69@gmail.com>
2026-04-01 18:10:18 +0300

    Update script.js

--- registration/script.js
+++ registration/script.js
@@ -3,6 +3,35 @@
 function registrationForm() {
     $("#registration_button").on("click", function() {
 
+        const users = {
+                       254712345001 : {
+                phone : "254712345001",
+                plate : "KDM 003Y",
+                license : "PAUL123"
+            },
+                        254712345002 : {
+                phone : "254712345002",
+                plate : "KDP 548N",
+                license : "PETER123"
+            },
+                        254712345003 : {
+                phone : "254712345003",
+                plate : "KAZ 611A",
+                license : "MARY123"
+            },
+                        254712345004 : {
+                phone : "254712345004",
+                plate : "KBQ 327T",
+                license : "NELLY123"
+            },
+                        254712345678 : {
+                phone : "254712345678",
+                plate : "KAA 123A",
+                license : "JOHN123"
+            }
+
+        };
+
         var $this = $("#registration_button"); //submit button selector using ID
         var $caption = $this.html(); // We store the html content of the submit button
         var form = "#registration_form"; //defined the #form ID
