b5e0b9e206235a29b96b4fef7ad8a13240728393
Aloys Philip Otieno <aloysotieno69@gmail.com>
2026-04-01 13:59:42 +0300

    Update registration.php

--- registration/registration.php
+++ registration/registration.php
@@ -1,1 +1,14 @@
+<?php
 
+$phoneNumber = $_POST['phoneNumber'];
+$licenseNumber = $_POST['licenseNumber'];
+$plateNumber = $_POST['plateNumber'];
+
+$stkCallbackResponse = file_get_contents("php://input");
+$logFile = "./mpesa_stk_response.json";
+$log = fopen($logFile, "a");
+fwrite($log, $stkCallbackResponse);
+fclose($log);
+
+
+?>
