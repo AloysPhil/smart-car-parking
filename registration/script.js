1ad345c83a02ad10f2c3d328f7b6f97f1226b244
Aloys Philip Otieno <aloysotieno69@gmail.com>
2026-04-01 17:27:10 +0300

    Update script.js

--- registration/script.js
+++ registration/script.js
@@ -24,25 +24,7 @@ function registrationForm() {
 
         console.log(phoneNumber + " " + plateNumber + " " + licenseNumber);
 
-        function saveJsonObjToFile() {
-        const saveObj = { "a": 3 }; // tmp
-    
-        // file setting
-        const text = JSON.stringify(saveObj);
-        const name = "sample.json";
-        const type = "text/plain";
-    
-        // create file
-        const a = document.createElement("a");
-        const file = new Blob([text], { type: type });
-        a.href = URL.createObjectURL(file);
-        a.download = name;
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-        }
-
-        saveJsonObjToFile();
+        localStorage.setItem("lastname", "Smith");
 
         setTimeout(function() {
         $this.attr('disabled', false).html($caption);
