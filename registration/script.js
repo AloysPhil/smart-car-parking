c53cc432dc4708bb84236c42408cd975611f3d46
Aloys Philip Otieno <aloysotieno69@gmail.com>
2026-04-02 14:32:08 +0300

    Update script.js

--- registration/script.js
+++ registration/script.js
@@ -3,31 +3,30 @@
 function registrationForm() {
     $("#registration_button").on("click", function() {
 
-        const users = [
-                                        {
-                phone : "254712345001",
-                plate : "KDM 003Y",
-                license : "PAUL123"
+        const users = [{
+                phone: "254712345001",
+                plate: "KDM 003Y",
+                license: "PAUL123"
             },
-                                        {
-                phone : "254712345002",
-                plate : "KDP 548N",
-                license : "PETER123"
+            {
+                phone: "254712345002",
+                plate: "KDP 548N",
+                license: "PETER123"
             },
-                                        {
-                phone : "254712345003",
-                plate : "KAZ 611A",
-                license : "MARY123"
+            {
+                phone: "254712345003",
+                plate: "KAZ 611A",
+                license: "MARY123"
             },
-                                        {
-                phone : "254712345004",
-                plate : "KBQ 327T",
-                license : "NELLY123"
+            {
+                phone: "254712345004",
+                plate: "KBQ 327T",
+                license: "NELLY123"
             },
-                                        {
-                phone : "254712345678",
-                plate : "KAA 123A",
-                license : "JOHN123"
+            {
+                phone: "254712345678",
+                plate: "KAA 123A",
+                license: "JOHN123"
             }
 
         ];
@@ -39,53 +38,63 @@ function registrationForm() {
         var route = $(form).attr('action'); //get the route using attribute action
 
         $this.attr('disabled', true).html("Processing...");
-
-        var mobile = $("#phoneNumber").val();
-
-        function setCharAt(str, index, chr) {
-            if (index > str.length - 1) return str;
-            return str.substring(0, index) + chr + str.substring(index + 1);
-        }
-
-        var phoneNumber = setCharAt(mobile, 0, "254");
-        var plateNumber = $("#plateNumber").val();
-        var licenseNumber = $("#licenseNumber").val();
-
-        console.log(phoneNumber + " " + plateNumber + " " + licenseNumber);
-
-        const userObj = {
-          phone: phoneNumber,
-          plate: plateNumber,
-          license: licenseNumber
-        };
-
-        var available = false;
-
-        for (let i = 0; i < users.length ; i++) {
-            
-            if(users[i].phone == phoneNumber ){
-
-                available = true;
-            }
-            
-        }
-
-        if(available === true){
-
-            alert("User already exists!");
-        }else{
-
-            localStorage.setItem(phoneNumber, JSON.stringify(userObj));
-        }
-
-     //   let newObject = localStorage.getItem(phoneNumber);
-     //  console.log(JSON.parse(newObject));
+        $("#processingModal").modal('show');
 
         setTimeout(function() {
-        $this.attr('disabled', false).html($caption);
+
+            var mobile = $("#phoneNumber").val();
+
+            function setCharAt(str, index, chr) {
+                if (index > str.length - 1) return str;
+                return str.substring(0, index) + chr + str.substring(index + 1);
+            }
+
+            var phoneNumber = setCharAt(mobile, 0, "254");
+            var plateNumber = $("#plateNumber").val();
+            var licenseNumber = $("#licenseNumber").val();
+
+            console.log(phoneNumber + " " + plateNumber + " " + licenseNumber);
+
+            const userObj = {
+                phone: phoneNumber,
+                plate: plateNumber,
+                license: licenseNumber
+            };
+
+            var available = false;
+
+            for (let i = 0; i < users.length; i++) {
+
+                if (users[i].phone == phoneNumber) {
+
+                    available = true;
+                }
+
+            }
+
+            if (available === true) {
+
+                setTimeout(function() {
+
+                    $("#notificationModal").modal('show');
+
+                }, 3000);
+
+            } else {
+
+                localStorage.setItem(phoneNumber, JSON.stringify(userObj));
+            }
+
+            //   let newObject = localStorage.getItem(phoneNumber);
+            //  console.log(JSON.parse(newObject));
+
+            setTimeout(function() {
+                $this.attr('disabled', false).html($caption);
+            //    $("#processingModal").modal('hide');
+
+            }, 3000);
 
         }, 3000);
-
 
     });
 }
